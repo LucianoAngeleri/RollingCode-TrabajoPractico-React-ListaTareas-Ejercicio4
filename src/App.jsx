896537f2a6d48f1a43f0e80cb3744fef102ee308
Luciano Angeleri <lcnang@gmail.com>
2023-06-01 20:19:55 -0300

    Se agregó la estructura del proyecto y el readme

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
